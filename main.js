a296a01b8d57390678e9b3282be5ba6dd820761f
aleruiz <ales.email@gmail.com>
2022-06-22 11:39:40 -0400

    WIP - ran into CORS trouble with getting data for OCR

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
+// platform agnostic shenanigans
+if (typeof browser === "undefined") {
+    var browser = chrome;
+}
+
+
 document.body.style.background = 'yellow';
-console.log('HELLO RUNNING');
-
-
+const worker = Tesseract.createWorker({
+  workerPath: browser.runtime.getURL('js/worker.min.js'),
+  langPath: browser.runtime.getURL('data'),
+  corePath: browser.runtime.getURL('js/tesseract-core.wasm.js'),
+});
+Tesseract.setLogging(true);
 
 async function removeIfTweet(e){
   let imageUrl = $(e).parent().parent().siblings('.thumbnail').attr('href');
   console.log(e);
   console.log(imageUrl);
   console.log('--------');
-  const worker = createWorker({
-    workerPath: browser.runtime.getURL('js/worker.min.js'),
-    langPath: browser.runtime.getURL('traineddata'),
-    corePath: browser.runtime.getURL('js/tesseract-core.wasm.js'),
-  });
   console.log(worker);
-  await worker.load();
+  console.log('z');
+  await worker.load()
+  console.log('a');
   await worker.loadLanguage('eng');
+    console.log('b');
   await worker.initialize('eng');
+  console.log('c');
   const { data: { text } } = await worker.recognize(imageUrl);
+  console.log('d');
   console.log('----------------------');
   console.log('RESULTS FOR IMAGE');
   console.log(imageUrl)
